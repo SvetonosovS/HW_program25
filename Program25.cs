0894973229a170f43fae2bf1ff154fb9d6a9eab9
SvetonosovS <tris23@bk.ru>
2022-10-15 16:45:57 +0300

    prog25

---
+++ Program25.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Clear();
+
+int enterNumberA = GetNumberFromUser("Введите первое целое число: ","Ошибка ввода!");
+int enterNumberB = GetNumberFromUser("Введите второе целое число: ","Ошибка ввода!");
+int result = enterNumberA;
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+      {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
+
+for (int i = 2; i <= enterNumberB; i++)
+    {
+     result *= enterNumberA;
+     }
+
+Console.WriteLine($"{enterNumberA}, {enterNumberB} -> {result}");          
